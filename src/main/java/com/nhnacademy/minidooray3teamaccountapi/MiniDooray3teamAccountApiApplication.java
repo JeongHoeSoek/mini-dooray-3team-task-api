6e41c9b28882529e5644c0667395d75b0843dbc2
정회석 <jeonghoe@jeonghoeseog-ui-noteubug.local>
2024-11-28 10:36:49 +0900

    first

---
+++ src/main/java/com/nhnacademy/minidooray3teamaccountapi/MiniDooray3teamAccountApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray3teamaccountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDooray3teamAccountApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDooray3teamAccountApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray3teamaccountapi/MiniDooray3teamAccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray3teamaccountapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDooray3teamAccountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
