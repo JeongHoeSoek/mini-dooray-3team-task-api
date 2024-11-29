94d6cfbf48d398f93392ac1c31ad08f51949df36
minkyuSnow <bingseol99@gmail.com>
2024-11-29 18:53:50 +0900

    MileStone에 Setter 추가

--- src/main/java/com/nhnacademy/minidooray3teamaccountapi/entity/MileStone.java
+++ src/main/java/com/nhnacademy/minidooray3teamaccountapi/entity/MileStone.java
@@ -1,14 +1,20 @@
 package com.nhnacademy.minidooray3teamaccountapi.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import lombok.Setter;
 
 @Entity
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
